e35b1e8ab264723702a88e9a1765d125befddaf3
GeodeArc <174155806+GeodeArc@users.noreply.github.com>
2026-04-10 16:38:17 +1200

    rtc module progress

--- main.ino
+++ main.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_SSD1306.h>
 #include <SoftwareSerial.h>
 #include <TimeLib.h>
+#include <RTClib.h>
 #include "DFRobotDFPlayerMini.h"
 
 // PINS
@@ -15,54 +16,63 @@ const int MP3_TX_PIN = 2;
 const int BT_RX_PIN = 6;
 const int BT_TX_PIN = 5;
 
-// DISPLAY
+// DEFINITIONS
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
+DFRobotDFPlayerMini MP3player;
+RTC_DS3231 rtc;
 
 // SERIAL
 SoftwareSerial SerialBT(BT_RX_PIN, BT_TX_PIN);
 SoftwareSerial SerialMP3(MP3_RX_PIN, MP3_TX_PIN);
-DFRobotDFPlayerMini MP3player;
 
 // LOGIC
 unsigned long prevMillis = 0;
+unsigned long menuNum = 1; 
+bool menuActive = true;
 bool isAlarmPlaying = false;
 bool showSnooze = false;
+bool secondsEnabled = false;
 unsigned long snoozeTimer = 0;
 
 // SETUP
 void setup() {
   Serial.begin(9600);
+  
+  if (!rtc.begin()) {
+    Serial.println(F("RTC Error"));
+  } else {
+    DateTime now = rtc.now();
+    setTime(now.unixtime());
+    Serial.println(F("RTC OK"));
+  }
+
   SerialBT.begin(9600);
   SerialMP3.begin(9600);
-
+  
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.display();
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
 
-  setTime(0, 0, 0, 1, 1, 2026);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(15,28);
+  display.print(F("Loading Clock :3"));
+  display.display();
+  delay(1000);
 
   SerialMP3.listen();
   if (MP3player.begin(SerialMP3, false, false)) {
-    MP3player.volume(30);
+    MP3player.volume(28);
   }
   SerialBT.listen();
 }
 
-void alarmPlay() {
-  Serial.println(F("Starting Alarm..."));
-  
-  SerialMP3.listen();
-  delay(50);
-  MP3player.play(1);
-  delay(50);
-  
-  SerialBT.listen();
-  isAlarmPlaying = true;
-}
-
+// MAIN LOOP
 void loop() {
   unsigned long currentMillis = millis();
 
@@ -70,16 +80,22 @@ void loop() {
   if (SerialBT.available()) {
     String msg = SerialBT.readStringUntil('\n');
     msg.trim();
+    
     if (msg.equals("alarm")) {
       alarmPlay();
     }
-    if (msg.startsWith("T")) setTime(msg.substring(1).toInt());
+    
+    if (msg.startsWith("T")) {
+      long t = msg.substring(1).toInt();
+      setTime(t);
+      rtc.adjust(DateTime(t));
+      Serial.println(F("Time Synced via Phone"));
+    }
   }
 
-  // ULTRASONIC SENSOR
+  // ULTRASONIC SENSOR (SNOOZE)
   if (currentMillis - prevMillis >= 150) {
     prevMillis = currentMillis;
-
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
@@ -90,18 +106,7 @@ void loop() {
     int distance = (duration * 0.034) / 2;
 
     if (distance > 0 && distance < 20 && isAlarmPlaying) {
-      Serial.println(F("Snooze Triggered"));
-      
-      SerialMP3.listen();
-      delay(50);
-      MP3player.stop();
-      
-      isAlarmPlaying = false; 
-      showSnooze = true;
-      snoozeTimer = currentMillis;
-      
-      SerialBT.listen();
-      displayTime();
+      triggerSnooze(currentMillis);
     }
   }
 
@@ -119,30 +124,81 @@ void loop() {
   }
 }
 
+void alarmPlay() {
+  SerialMP3.listen();
+  delay(50);
+  MP3player.play(1);
+  delay(50);
+  SerialBT.listen();
+  isAlarmPlaying = true;
+}
+
+void triggerSnooze(unsigned long currentMillis) {
+  SerialMP3.listen();
+  delay(50);
+  MP3player.stop();
+  isAlarmPlaying = false; 
+  showSnooze = true;
+  snoozeTimer = currentMillis;
+  SerialBT.listen();
+  displayTime();
+}
+
 void displayTime() {
   display.clearDisplay();
   display.setTextColor(WHITE);
-  
-  display.setCursor(0, 15); 
-  display.setTextSize(3);
-  if (hour() < 10) display.print("0"); display.print(hour());
-  display.print(":");
-  if (minute() < 10) display.print("0"); display.print(minute());
-  
-  display.setTextSize(2);
-  display.print(" "); 
-  if (second() < 10) display.print("0"); display.print(second());
 
+  // MENU
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print("CLOCK");
+
+  display.setCursor(40, 0);
+  display.print("TIMER");
+
+  display.setCursor(80, 0);
+  display.print("STPWATCH");
+
+  if (menuActive) {
+    if (menuNum == 1) {
+      display.drawLine(0, 9, 29, 9, WHITE);
+    }
+    if (menuNum == 2) {
+      display.drawLine(40, 9, 68, 9, WHITE);
+    }
+    if (menuNum == 3) {
+      display.drawLine(80, 9, 126, 9, WHITE);
+    }
+  }
+
+  // CLOCK
+  display.setTextSize(3);
+  if (secondsEnabled) {
+      display.setCursor(0, 20); 
+
+      if (hour() < 10) display.print("0"); display.print(hour());
+      display.print(":");
+      if (minute() < 10) display.print("0"); display.print(minute());
+      
+      display.setTextSize(2);
+      display.print(" "); 
+      if (second() < 10) display.print("0"); display.print(second());
+  } else {
+    display.setCursor(20, 20); 
+    
+    if (hour() < 10) display.print("0"); display.print(hour());
+    display.print(":");
+    if (minute() < 10) display.print("0"); display.print(minute());
+  }
+
+  // NOTIFICATIONS
   if (isAlarmPlaying) {
-    display.setTextSize(1);
-    display.setCursor(25, 52);
+    display.setCursor(25, 55); display.setTextSize(1);
     display.print(F("ALARM PLAYING"));
   }
   if (showSnooze) {
-    display.setTextSize(1);
-    display.setCursor(25, 52);
+    display.setCursor(25, 55); display.setTextSize(1);
     display.print(F("ALARM STOPPED"));
-
   }
   display.display();
 }
